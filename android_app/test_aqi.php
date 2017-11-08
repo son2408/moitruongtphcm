68608ae304ef80da42a3bc89eedf54e1c773bf76
tandat <datca77@gmail.com>
2017-11-08 09:59:28 +0700

    edit test_aqi.php

--- android_app/test_aqi.php
+++ android_app/test_aqi.php
@@ -4,10 +4,6 @@ include("config.inc.php");
 $thoigian= pg_query($link, "select thoigian from aqi_hcm_thang3_2011");
 $mang =array();
 
-while($row = pg_fetch_row($ketqua)){
-	$aqi 		= 	$row[0];
-	$thoigian 	= 	$row[1];
-
 $arr = pg_fetch_all($ketqua);
 	echo json_encode($arr);
 ?>
