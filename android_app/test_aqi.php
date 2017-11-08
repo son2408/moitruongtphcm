0bc970816f6963d6565f376419384147936380ee
tandat <datca77@gmail.com>
2017-11-08 10:48:52 +0700

    edit test_aqi.php

--- android_app/test_aqi.php
+++ android_app/test_aqi.php
@@ -1,7 +1,7 @@
 <?php
 include("config.inc.php");
 
-$tentram= pg_query($link, "SELECT DISTINCT tentram, from aqi_hcm_thang3_2011_ngay");
+$tentram= pg_query($link, "SELECT DISTINCT tentram from aqi_hcm_thang3_2011_ngay");
 $arr = pg_fetch_all($tentram);
 	echo json_encode($arr);
 ?>
