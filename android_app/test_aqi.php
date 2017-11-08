0a68c5f900b07d9dbedc19a6aa832e505020d7e5
tandat <datca77@gmail.com>
2017-11-08 09:54:38 +0700

    add testaqi.php

---
+++ android_app/test_aqi.php
@@ -0,0 +1,16 @@
+<?php
+include("config.inc.php");
+
+
+$sql = $_POST['sql'];
+
+$thoigian= pg_query($link, "select thoigian from aqi_hcm_thang3_2011");
+$mang =array();
+
+while($row = pg_fetch_row($ketqua)){
+	$aqi 		= 	$row[0];
+	$thoigian 	= 	$row[1];
+
+$arr = pg_fetch_all($ketqua);
+	echo json_encode($arr);
+?>
