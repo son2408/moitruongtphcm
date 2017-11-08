b97b142e6f174bd9f0a753696d3933cdf6be035d
tandat <datca77@gmail.com>
2017-11-08 15:48:46 +0700

    edit test_aqi_get_date.php

--- android_app/test_aqi_get_date.php
+++ android_app/test_aqi_get_date.php
@@ -1,9 +1,9 @@
 <?php
 include("config.inc.php");
 
-$tentram = $_POST["station_name"]
+//$tentram = $_POST["station_name"]
 
-$thoigian= pg_query($link, "SELECT DISTINCT thoigian from aqi_hcm_thang3_2011_ngay WHERE tentram = ".$tentram  );
+$thoigian= pg_query($link, "SELECT DISTINCT thoigian from aqi_hcm_thang3_2011_ngay WHERE tentram = 'Hàng Xanh'"  );
 $arr = pg_fetch_all($thoigian);
 	echo json_encode($arr);
 ?>
