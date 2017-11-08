d5df64cb8e01567c8d36f44fa82b315769c5366f
tandat <datca77@gmail.com>
2017-11-08 10:47:38 +0700

    edit test_aqi.php

--- android_app/test_aqi.php
+++ android_app/test_aqi.php
@@ -1,7 +1,7 @@
 <?php
 include("config.inc.php");
 
-$thoigian= pg_query($link, "SELECT DISTINCT thoigian, tentram from aqi_hcm_thang3_2011_ngay");
-$arr = pg_fetch_all($thoigian);
+$tentram= pg_query($link, "SELECT DISTINCT tentram, from aqi_hcm_thang3_2011_ngay");
+$arr = pg_fetch_all($tentram);
 	echo json_encode($arr);
 ?>
