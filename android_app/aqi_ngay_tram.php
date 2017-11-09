a87348da0068ecaf9cf6c6384b24a7bc885cae48
tandat <datca77@gmail.com>
2017-11-09 16:47:35 +0700

    aaa

--- android_app/aqi_ngay_tram.php
+++ android_app/aqi_ngay_tram.php
@@ -5,7 +5,7 @@ include("config.inc.php");
 $tentram = $_POST['station_name'];
 $thoigian = $_POST['time'];
 
-$aqi= pg_query($link, "SELECT aqi_ngay_tram
+$aqi= pg_query($link, "SELECT aqi_ngay_tram, thongso_h_max
 						from aqi_hcm_thang3_2011_ngay
 						WHERE tentram = "."'".$tentram."'".
 						" and thoigian = "."'".$thoigian."'");
