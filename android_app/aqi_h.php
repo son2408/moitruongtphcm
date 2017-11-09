fe953c2dd395a320fcb770d510ca9f15d7743598
tandat <datca77@gmail.com>
2017-11-09 15:34:16 +0700

    aaa

--- android_app/aqi_h.php
+++ android_app/aqi_h.php
@@ -7,7 +7,7 @@ $aqi= pg_query($link, "SELECT thoigian::time, aqi_h_tram, aqi_h_so2, aqi_h_co, a
 									aqi_h_o3, aqi_h_tsp, aqi_h_pm25, aqi_h_pm10, aqi_h_pb, thongso_h_max
 							FROM aqi_hcm_thang3_2011 
 							WHERE tentram = "."'".$tentram."'".
-							" and thoigian::date = "."'".$thoigian."'");
+							" and thoigian::date = "."'".$thoigian."'"." Order by thoigian::time");
 
 // $aqi= pg_query($link, "SELECT thoigian, aqi_h_tram, aqi_h_so2, aqi_h_co, aqi_h_no2,
 // 									aqi_h_o3, aqi_h_tsp, aqi_h_pm25, aqi_h_pm10, aqi_h_pb, thongso_h_max
