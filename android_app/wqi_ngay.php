31374a815937abd0a2b7f727713bf380e4343e61
tandat <datca77@gmail.com>
2017-12-01 14:29:11 +0700

    dfuygihojp

--- android_app/wqi_ngay.php
+++ android_app/wqi_ngay.php
@@ -4,7 +4,7 @@ $tentram = "Cát Lái";//$_POST['station_name'];
 $month = 9;//$_POST['month'];
 $year = 2014;//$_POST['year'];
 
-$aqi= pg_query($link, "SELECT * from wqi_hcm_2014 where Extract(month from thoigian) = ".$month." and Extract(year from thoigian) = ".$year );
+$aqi= pg_query($link, "SELECT * from wqi_hcm_2014 where tentram = ".$tentram." and Extract(month from thoigian) = ".$month." and Extract(year from thoigian) = ".$year );
 
 $arr = pg_fetch_all($aqi);
 	echo json_encode($arr);
