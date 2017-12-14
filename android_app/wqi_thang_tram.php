f410c4acb9f30179823ec6796a335f2506fcaced
tandat <datca77@gmail.com>
2017-12-14 15:55:51 +0700

    adgfhy

--- android_app/wqi_thang_tram.php
+++ android_app/wqi_thang_tram.php
@@ -5,7 +5,7 @@ include("config.inc.php");
 $tentram = $_POST['station_name'];
 $thoigian = $_POST['time'];
 
-$wqi= pg_query($link, "SELECT wqi_tram
+$wqi= pg_query($link, "SELECT x, y, wqi_tram
 						from wqi_hcm_2014_thang
 						WHERE tentram = "."'".$tentram."'".
 						" and thoigian = "."'".$thoigian."'");
